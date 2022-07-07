82f188a930b2740ae75a59810ff289b3ea6d651a
Ryan Mullen <ryanfmullen@gmail.com>
2022-07-07 14:42:30 -0700

    finish phase 1

--- lib/first_tdd_project.rb
+++ lib/first_tdd_project.rb
@@ -8,3 +8,43 @@ def my_uniq(arr)
     end
     new_arr
 end
+
+class Array
+
+    def two_sum
+        new_arr = []
+        (0...self.length).each do |i|
+            (i+1...self.length).each do |j|
+                if self[i] + self[j] == 0
+                    new_arr << [i, j]
+                end
+            end
+        end
+        new_arr
+    end
+end
+
+def my_transpose(grid)
+    arr = []
+    (0...grid.length).each do |i|
+        temp = []
+        grid.each_with_index do |subarr, j|
+            temp << subarr[i]
+        end
+        arr << temp
+    end
+    arr
+end
+
+def stock_picker(arr)
+    hash = Hash.new {|h,k| h[k] = []}
+
+    (0...arr.length).each do |i|
+        (i+1...arr.length).each do |j|
+            hash[[i,j]] = arr[j] - arr[i]
+        end
+    end
+    hash.sort_by {|k,v| v}[-1][0]
+end
+
+
--- spec/first_tdd_project_spec.rb
+++ spec/first_tdd_project_spec.rb
@@ -23,10 +23,39 @@ end
 
 describe Array do
     let(:arr) {[-1,-5,2,1,4,5]}
-
-
     it 'should accept an array and return a new array' do
-        
+        expect(arr.two_sum).to_not be(arr)
     end
-    
+
+    it 'should return the index where elements add to 0' do
+        expect(arr.two_sum).to eq([[0,3],[1,5]])
+    end
 end
+
+describe 'my_transpose' do
+    let(:rows) {[[0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]]}
+    let(:col) {[
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+      ]}
+    it 'should not use built in method Array#transpose' do
+        expect(rows).to_not receive(:transpose)
+    end
+
+    it 'should transpose the matrix' do
+        expect(my_transpose(rows)).to eq(col)
+    end
+end
+
+describe 'stock_picker' do
+    let(:arr) {[2,1,3,5,2,4,9,8,7]}
+
+    it 'should return the indexes of which pair has the greatest difference' do 
+        expect(stock_picker(arr)).to eq([1,6])
+    end
+
+end
+
